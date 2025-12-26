39fedcb6731a3afd4a883de3a1021582e125fb6c
ITG DEV <glennbancolita@cvmfinance.com>
2025-12-26 15:58:06 +0800

    Updated route.ts

--- src/app/api/auth/me/route.ts
+++ src/app/api/auth/me/route.ts
@@ -17,7 +17,7 @@ export async function GET() {
       );
     }
 
-    const user = await prisma.user.findUnique({
+    const user = await prisma.user.findFirst({
       where: { email: session.email },
       select: {
         id: true,
