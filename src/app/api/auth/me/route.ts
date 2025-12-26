9fffea9a3913bfd713df159fae4c350f29db92d2
ITG DEV <glennbancolita@cvmfinance.com>
2025-12-26 15:54:29 +0800

    Updated route.ts

--- src/app/api/auth/me/route.ts
+++ src/app/api/auth/me/route.ts
@@ -9,7 +9,7 @@ export async function GET() {
  
   try {
     const session = await getServerSession();
-   
+   console.log('sdasd')
     if (!session) {
       return NextResponse.json(
         { error: 'Not authenticated' },
