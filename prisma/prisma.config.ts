7bb06189a554317cce6ab548f60d9367c5ccf6d2
ITG DEV <glennbancolita@cvmfinance.com>
2025-12-29 09:42:54 +0800

    Updated prisma.config.ts

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -1,26 +1,13 @@
-// import path from 'node:path';
-// import { defineConfig } from 'prisma/config';
-
-// export default defineConfig({
-//   schema: path.join(__dirname, 'schema.prisma'),
-//   datasource: {
-//     url: process.env.DATABASE_URL || '',
-//   },
-// });
-import path from 'node:path';
-import { defineConfig } from 'prisma/config';
-import { PrismaNeon } from '@prisma/adapter-neon';
-import { Pool } from '@neondatabase/serverless';
+import path from 'node:path'
+import { defineConfig } from 'prisma/config'
 
 if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL environment variable is missing in Bolt');
+  throw new Error('DATABASE_URL is missing')
 }
-console.log( path.join(__dirname, 'schema.prisma'))
+
 export default defineConfig({
   schema: path.join(__dirname, 'schema.prisma'),
   datasource: {
-    adapter: new PrismaNeon(
-      new Pool({ connectionString: process.env.DATABASE_URL }) as never
-    ),
+    url: process.env.DATABASE_URL,
   },
-});
+})
